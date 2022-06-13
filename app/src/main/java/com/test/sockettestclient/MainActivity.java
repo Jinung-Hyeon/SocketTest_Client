cb6acdf93ee7360352f1b93ed2aedbd03291931d
Jinung Hyeon <oiuoiyu@naver.com>
2022-06-14 00:13:36 +0900

    홈버튼 눌러서 나가졌을때 다시 이 앱이 실행 되는 코드 구현 꺼지면 와치독 앱을 호출하고 와치독이 다시 이앱을 실행시키는 구조

--- app/src/main/java/com/test/sockettestclient/MainActivity.java
+++ app/src/main/java/com/test/sockettestclient/MainActivity.java
@@ -2,7 +2,11 @@ package com.test.sockettestclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,37 +19,44 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "test";
+
+    //로컬 IP사용시 127.0.0.1로 ip 변경하기
+    private final String ip = "192.168.123.104";
+    private final String port = "5002";
+
     Socket socket;     //클라이언트의 소켓
+
     DataInputStream is;
     DataOutputStream os;
 
-    String ip;
-    String port;
-
-    TextView text_msg;  //서버로 부터 받은 메세지를 보여주는 TextView
-    EditText edit_msg;  //서버로 전송할 메세지를 작성하는 EditText
-    EditText edit_ip;   //서버의 IP를 작성할 수 있는 EditText
-    EditText edit_port;
-    Button btn_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit_ip = findViewById(R.id.ip);
-        edit_port = findViewById(R.id.port);
-        btn_connect = findViewById(R.id.connect);
-        edit_msg = findViewById(R.id.msg);
-        text_msg = findViewById(R.id.chatting);
+
+        ClientSocketOpen();
     }
 
-    public void ClientSocketOpen(View view) {
-        ip = "192.168.0.50";
-        port = "5001";
-        //ip = edit_ip.getText().toString(); //EditText에 적은 IP주소 얻어오기
-        //port = edit_port.getText().toString();
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            socket.close(); //소켓을 닫는다.
+            moveTaskToBack(true); // 태스크를 백그라운드로 이동
+            finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+            android.os.Process.killProcess(android.os.Process.myPid()); //앱 프로세스 종료
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ClientSocketOpen() {
 
         if (ip.isEmpty() || port.isEmpty()) {
             Toast.makeText(MainActivity.this, "ip주소와 포트번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
@@ -74,17 +85,47 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }).start();
         }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public boolean moveTaskToBack(boolean nonRoot) {
+        return super.moveTaskToBack(nonRoot);
+    }
+
+    @Override
+    public void finishAndRemoveTask() {
+        super.finishAndRemoveTask();
+    }
+
+    @Override
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+        getPackageList();
+        Log.d(TAG, "homeKey: ");
+    }
+
+    public void getPackageList() {
+        //SDK30이상은 Manifest권한 추가가 필요 출처:https://inpro.tistory.com/214
+        PackageManager pkgMgr = getPackageManager();
+        List<ResolveInfo> mApps;
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
+
         try {
-            socket.close(); //소켓을 닫는다.
-        } catch (IOException e) {
+            for (int i = 0; i < mApps.size(); i++) {
+                if(mApps.get(i).activityInfo.packageName.startsWith("com.test.sokettestserver")){
+                    Log.d("start", "실행시킴");
+                    break;
+                }
+            }
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.test.sokettestserver");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
